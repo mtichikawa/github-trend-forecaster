e4de13d1c316b9809cc8ae3bb56c121f4a30789c
Mike Ichikawa <projects.ichikawa@gmail.com>
2026-03-04 10:51:13 -0800

    Handle RateLimitExceededException in GitHubDataCollector methods

--- src/data_collector.py
+++ src/data_collector.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 from pathlib import Path
 import time
 
-from github import Github, RateLimitExceededException  # RateLimitExceededException handled in collect_multiple()
+from github import Github, RateLimitExceededException  # RateLimitExceededException handled in collect_multiple()  # RateLimitExceededException handled in collect_multiple()
 import pandas as pd
 
 
